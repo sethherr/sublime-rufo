88587eb8156b3df5e01642913afd2e0339380eed
sethherr <seth.william.herr@gmail.com>
2019-04-24 12:40:32 -0700

    wip

--- rufo_format.py
+++ rufo_format.py
@@ -1,53 +1,80 @@
-import sublime_plugin
-import sublime
+import os
+import fnmatch
 import subprocess
-from .diff_match_patch import diff_match_patch
-import json
+import sublime
+import sublime_plugin
+
 
 class RufoPluginListener(sublime_plugin.EventListener):
   def on_pre_save(self, view):
-    settings = sublime.load_settings('sublime-rufo.sublime-settings')
-    if settings.get('auto_format') == None or settings.get('auto_format') == True:
-      view.run_command('rufo_format')
+    view.run_command("rufo")
 
-class RufoFormatCommand(sublime_plugin.TextCommand):
-  def is_enabled(self):
-    caret = self.view.sel()[0].a
-    syntax_name = self.view.scope_name(caret)
-    return "source.ruby" in syntax_name
-
-  def has_redo(self):
-    cmd, args, repeat = self.view.command_history(1)
-    return cmd != ''
-
+class RufoCommand(sublime_plugin.TextCommand):
   def run(self, edit):
-    vsize = self.view.size()
-    region = sublime.Region(0, vsize)
-    src = self.view.substr(region)
     window = self.view.window()
     filename = self.view.file_name()
+    # I was initially thinking of running the command from the project root, but I think that could introduce bugs
+    # Instead, just use the folder of the file
+    project_variables = window.extract_variables()
+    command_dir = project_variables["file_path"]
+    filename_matchers = ["*.rb", "*.rake", "*gemfile"]
+    # Check if the current filename matches any of our filname matcher globs
+    if any(fnmatch.fnmatch(filename.lower(), glob) for glob in filename_matchers):
+      print("Matching filename")
+      os.chdir(command_dir)
+      command = "bundle exec rufo '" + filename + "'"
+      print(command, command_dir)
+      print(subprocess.check_output("ruby --version", shell = True))
+      # subprocess.check_call('rvm env --path -- ruby-version[@gemset-name]')
+      # output = subprocess.check_output(command, shell = True)
 
-    settings = sublime.load_settings('sublime-rufo.sublime-settings')
-    rufo_cmd = settings.get("rufo_cmd") or "rufo"
+      # vsize = self.view.size()
+      # region = sublime.Region(0, vsize)
+      # src = self.view.substr(region)
+      # with subprocess.Popen("rufo", stdin = subprocess.PIPE, stdout = subprocess.PIPE, shell = True) as proc:
+      #   proc.stdin.write(bytes(src, "UTF-8"))
+      #   proc.stdin.close()
+      #   output = proc.stdout.read().decode("UTF-8")
+      #   exit = proc.wait()
 
-    command = [rufo_cmd]
-    if filename != None:
-      command = [rufo_cmd, "--filename", filename]
+      # pos = 0
+      # print(exit)
+      # print(output)
+      # if exit == 3:
+      #   print("FFFFFF")
+      #   if not self.has_redo():
+      #     for op, text in diff_match_patch().diff_main(src, output):
+      #       if op == diff_match_patch.DIFF_DELETE:
+      #         self.view.erase(edit, sublime.Region(pos, pos + len(text)))
+      #       if op == diff_match_patch.DIFF_INSERT:
+      #         self.view.insert(edit, pos, text)
+      #         pos += len(text)
+      #       if op == diff_match_patch.DIFF_EQUAL:
+      #         pos += len(text)
 
-    with subprocess.Popen(command, stdin = subprocess.PIPE, stdout = subprocess.PIPE) as proc:
-      proc.stdin.write(bytes(src, 'UTF-8'))
-      proc.stdin.close()
-      output = proc.stdout.read().decode('UTF-8')
-      exit = proc.wait()
+      # output = subprocess.Popen(cmd, shell = True)
+      # proc.stdin.write(bytes(src, "UTF-8"))
+      # proc.stdin.close()
+      # output = proc.stdout.read().decode("UTF-8")
+      # exit = proc.wait()
+      print("Ran it")
+    else:
+      print("non-matching filename")
+    # print(fnmatch.fnmatch(filename.lower(), "*.rb"))
+    # if self.parseFilename("'" + filename + "'"):
+    # print("We're parsing!")
+    # For simplicity, just running the command in the folder of the file that was saved
+    # command_dir = self.view.filename
+    # command = [rufo_cmd, "--filename", filename]
+    # caret = self.view.sel()[0].a
+    # msg = self.view.scope_name(caret)
+    # msg = "'Hello, World!'\n"
 
-    pos = 0
-    if exit == 3:
-      if not self.has_redo():
-        for op, text in diff_match_patch().diff_main(src, output):
-          if op == diff_match_patch.DIFF_DELETE:
-            self.view.erase(edit, sublime.Region(pos, pos + len(text)))
-          if op == diff_match_patch.DIFF_INSERT:
-            self.view.insert(edit, pos, text)
-            pos += len(text)
-          if op == diff_match_patch.DIFF_EQUAL:
-            pos += len(text)
+    # project_data = sublime.Window.project_data(self)
+
+    # sublime.error_message(msg)
+    # os.chdir()
+    # print(project_variables)
+
+    # print(command_dir)
+    print()
